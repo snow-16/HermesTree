6c039c200d8727d3f5371a77cd5eae5f62ffe888
AizawaYuki <imagination1159@gmail.com>
2026-08-21 18:04:33 +0900

    [add]キーバインド追加

---
+++ Assets/Scripts/Enum/PlayerInputType.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// プレイヤー操作用の入力の種類
+/// </summary>
+public enum PlayerInputType
+{
+    MoveRight,
+    MoveLeft,
+}
--- Assets/Scripts/Player/PlayerCore.cs
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using R3;
+using System;
+using System.Collections.Generic;
 
 /// <summary>
 /// プレイヤーのプレイヤーの各処理を管理する中核コンポーネント
 /// </summary>
 public class PlayerCore : MonoBehaviour
 {
+    private Dictionary<PlayerInputType, Action> _inputActions = new();
+    public Dictionary<PlayerInputType, Action> InputActions { get => _inputActions; set => _inputActions = value; }
+
     void Start()
     {
+        var playerInputMap = InputSystem.actions.FindActionMap("Player");
         
+        foreach(PlayerInputType actionType in Enum.GetValues(typeof(PlayerInputType)))
+        {
+            _inputActions.Add(actionType, null);
+
+            Observable.EveryUpdate().Where(_ => playerInputMap[actionType.ToString()].IsPressed()).Subscribe(_ =>
+            {
+                _inputActions[actionType]?.Invoke();
+            }).AddTo(this);
+        }
+
+        _inputActions[PlayerInputType.MoveRight] += Move;
     }
 
-    void Update()
+    public void Move()
     {
-        if(Keyboard.current.dKey.isPressed)
-        {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10);
-        }
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10);
     }
 }
