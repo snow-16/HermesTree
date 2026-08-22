dd709c5deb9ba710febc5e7f87999cc800fd90e7
AizawaYuki <imagination1159@gmail.com>
2026-08-22 17:40:17 +0900

    [add]入力検知をより多態的に

--- Assets/Scripts/Enum/PlayerKeyBindType.cs
+++ Assets/Scripts/Enum/PlayerKeyBindType.cs
@@ -5,4 +5,5 @@ public enum PlayerKeyBindType
 {
     MoveRight,
     MoveLeft,
+    Jump,
 }
--- Assets/Scripts/Player/PlayerCore.cs
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -15,44 +15,61 @@ public class PlayerCore : MonoBehaviour
     void Awake()
     {
         var playerInputMap = InputSystem.actions.FindActionMap("Player");
-
-        var actionContainerTemplate = new Dictionary<InputType, ActionContainer>
-        {
-            { InputType.IsPressed, new(action => action.IsPressed()) },
-            { InputType.IsReleaced, new(action => !action.IsPressed()) },
-            { InputType.NowPressed, new(action => action.WasPressedThisFrame()) },
-            { InputType.NowReleaced, new(action => action.WasReleasedThisFrame()) }
-        };
         
         foreach(PlayerKeyBindType actionType in Enum.GetValues(typeof(PlayerKeyBindType)))
         {
+            var actionContainerTemplate = new Dictionary<InputType, ActionContainer>
+            {
+                { InputType.IsPressed, new(action => action.IsPressed()) },
+                { InputType.IsReleaced, new(action => !action.IsPressed()) },
+                { InputType.NowPressed, new(action => action.WasPressedThisFrame()) },
+                { InputType.NowReleaced, new(action => action.WasReleasedThisFrame()) }
+            };
             _inputActions.Add(actionType, actionContainerTemplate);
 
             foreach(InputType inputType in Enum.GetValues(typeof(InputType)))
             {
-                Observable.EveryUpdate().Where(_ => _inputActions[actionType][inputType].actionTrigger(playerInputMap[actionType.ToString()])).Subscribe(_ =>
+                var actionContainer = _inputActions[actionType][inputType];
+                var action = playerInputMap[actionType.ToString()];
+                Observable.EveryUpdate().Where(_ => actionContainer.action != null).Where(_ => actionContainer.actionTrigger(action)).Subscribe(_ =>
                 {
-                    _inputActions[actionType][inputType].action?.Invoke();
+                    actionContainer.action.Invoke(actionContainer.actionOutput(action));
                 }).AddTo(this);
             }
         }
-
-        _inputActions[PlayerKeyBindType.MoveRight][InputType.IsPressed].action += Move;
     }
 
-    public void Move()
+    /// <summary>
+    /// 入力を受け取るメソッドを追加する
+    /// </summary>
+    /// <param name="keyBindType">キーバインドの種類</param>
+    /// <param name="inputType">入力の種類</param>
+    /// <param name="inputAction">購読メソッド</param>
+    /// <param name="listenObject">メソッドの持ち主</param>
+    public void AddListener(PlayerKeyBindType keyBindType, InputType inputType, Action<Vector2> inputAction, Func<InputAction, Vector2> output, MonoBehaviour listenObject)
     {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10);
+        _inputActions[keyBindType][inputType].action += inputAction;
+        _inputActions[keyBindType][inputType].SetOutputProcess(output);
+        Observable.EveryUpdate().Where(_ => listenObject == null).Take(1).Subscribe(_ => _inputActions[keyBindType][inputType].action -= inputAction).AddTo(this);
     }
 
+    /// <summary>
+    /// 入力の種類ごとに処理を保持するクラス
+    /// </summary>
     public class ActionContainer
     {
-        public Action action = null;
+        public Action<Vector2> action = null;
         public readonly Func<InputAction, bool> actionTrigger;
+        public Func<InputAction, Vector2> actionOutput;
 
         public ActionContainer(Func<InputAction, bool> trigger)
         {
             actionTrigger = trigger;
+        }
+
+        public void SetOutputProcess(Func<InputAction, Vector2> output)
+        {
+            actionOutput = output;
         }
     }
 }
